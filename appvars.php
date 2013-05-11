6dc59af201ca8a4b212e2cb6af821c5ad08f3895
Joseph Rogina <joseph@rogina.net>
2013-05-10 20:01:21 -0600

    Create appvars.php

---
+++ appvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define application constants
+  define('UPLOADPATH', 'images/');
+  define('MAXFILESIZE', 32768);      // 32 KB
+  define('MAXIMGWIDTH', 50);         // 50 pixels
+  define('MAXIMGHEIGHT', 50);        // 50 pixels
+?>
